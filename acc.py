e56a85b92f19b27fc0bf150f721afb2e130250af
Felipe Rodas <68298003+dakkapik@users.noreply.github.com>
2022-06-03 13:48:46 -0400

    websocket test

--- acc.py
+++ acc.py
@@ -78,7 +78,7 @@ while True:
 	Gz = gyro_z/131.0
 	
 	f = open("giro.json", "w")
-	f.write("{Gx:" + str(Gx) + ", Gy:" + str(Gy) + ", Gz:" + str(Gz) +", Ax:" + str(Ax) +", Ay:"+ str(Ay) +",Az:"+ str(Az)+"}")
+	f.write('{"Gx":' + str(Gx) + ', "Gy":' + str(Gy) + ', "Gz":' + str(Gz) +', "Ax":' + str(Ax) +', "Ay":'+ str(Ay) +',"Az":'+ str(Az)+"}")
 	f.close()
 	sleep(0.2)
 	
---
+++ client.js
@@ -0,0 +1,15 @@
+const { io } = require("socket.io-client")
+const fs = require("fs")
+
+const socket = io ("http://192.168.0.210:5000")
+
+socket.on("connect",() => {
+    socket.emit("purpose", "giro")
+})
+
+setInterval(()=> {
+    const giro = fs.readFileSync("giro.json")
+    socket.emit("giro", JSON.parse(giro))
+}, 200)
+
+
