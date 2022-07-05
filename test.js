81ea644d5b7825db549ecfc1593fbb2ff38bd644
Felipe Rodas <68298003+dakkapik@users.noreply.github.com>
2022-07-05 16:41:00 -0400

    child test 1

---
+++ test.js
@@ -0,0 +1,52 @@
+'use strict'
+const { spawn, execSync } = require("child_process");
+const { Readable } = require("stream")
+const { unlink } = require('fs')
+const path = require("path")
+
+const childDir = path.join(__dirname, 'src')
+
+//*TODO:
+/// process to check install of wirepi and install
+
+execSync('gcc -Wall -o a mpu.c main.c -lwiringPi', {
+    'cwd': childDir
+}, (err, stdout, stderr) => {
+    if(!err) {
+        console.log('subprocess stdout: ', stdout.toString())
+        console.log('subprocess stderr: ', stderr.toString())
+        spawnA()
+    } else {
+        console.error("Subprocess error: ", err)
+    }
+})
+
+
+function spawnA(){
+    const child = spawn('a', {} ,{
+        stdio: ['ignore', 'pipe', 'inherit'],
+        cwd: childDir
+    })
+    
+    const readStream = Readable.from(child.stdout)
+    
+    readStream.on("data", (data)=> {
+        console.log(data.toString())
+    })
+    
+    readStream.on('error', (err) => {
+        console.error("child spawn error: ",err)
+    })
+
+    readStream.on("close", () => {
+        //cleanup
+        unlink(path.join(childDir, 'a'), (err) => {
+            if(!err) {
+                console.log("clean up successfull..")
+            } else {
+                console.error("error on cleanup: ", err)
+            }
+        })
+    })
+}
+
